8c0d706330f30efc9d06a816984db47bb4e3c3a2
1017114581@qq.com <1017114581@qq.com>
2017-11-10 15:49:19 +0800

    add

--- src/emq_kafka_app.erl
+++ src/emq_kafka_app.erl
@@ -1,23 +1,30 @@
 %% @author hebin
-%% @doc app define.
+%% @doc @todo Add description to emq_kafka_app.
+
 
 -module(emq_kafka_app).
 
 -behaviour(application).
 
-%% Application callbacks
+-include("emq_kafka.hrl").
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
 -export([start/2, stop/1]).
 
-%% ===================================================================
-%% Application callbacks
-%% ===================================================================
+
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
 
 start(_StartType, _StartArgs) ->
     {ok, Sup} = emq_kafka_sup:start_link(),
-	emq_kafka_config:init("test"),
-	%% emq_kafka:load(application:get_all_env()),
-	{ok, Sup}.
+	emq_kafka_hook:load(application:get_all_env()),
+    emq_kafka_config:register(),
+    {ok, Sup}.
 
 stop(_State) ->
-	emq_kafka:unload(),
-    ok.
+	emq_kafka_hook:unload(),
+    emq_kafka_config:unregister().
